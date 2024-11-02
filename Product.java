9ae690e393d8aa89ff2121c3af4d630f0425353b
Alex <asolovkina@gmail.com>
2024-11-02 13:09:51 +0300

    Product

---
+++ Product.java
@@ -0,0 +1,29 @@
+// Product.java
+public abstract class Product {
+    private String name;
+    private int price;
+    private int volume;
+
+    public Product(String name, int price, int volume) {
+        this.name = name;
+        this.price = price;
+        this.volume = volume;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    @Override
+    public String toString() {
+        return "Продукт: " + name + ", Цена: " + price + " руб., Объем: " + volume + " мл.";
+    }
+}
